cb74c939e314108057c21c93f0cb4063a71dd5ad
jayasuryaa-govindraj <jayasuryaa.govindraj1998@gmail.com>
2023-03-13 16:26:32 -0500

    Code cleanup.

--- frontend/src/utils/network.js
+++ frontend/src/utils/network.js
@@ -1,4 +1,4 @@
 const backendAPI = "https://splitify-backend.herokuapp.com";
-// const backendAPI = "http://127.0.0.1:5000";
+// const backendAPI = "http://127.0.0.1:8080";
 
 export default backendAPI;
