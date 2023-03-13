a4df1bc11ed04a6923cc81f58cb67a0ba5d49363
jayasuryaa-govindraj <jayasuryaa.govindraj1998@gmail.com>
2023-03-12 20:51:42 -0500

    Fixed the navbar icon alignment.

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,6 +4,7 @@ import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
 import { green } from "@mui/material/colors";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { Box } from "@mui/system";
 
 const NavBar = () => {
   const navigate = useNavigate();
@@ -32,27 +33,32 @@ const NavBar = () => {
   return (
     <AppBar position="static">
       <Toolbar sx={{ backgroundColor: green[600] }}>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="logo"
-          onClick={() => navigate("/")}
-        >
-          <AccountBalanceWalletRoundedIcon fontSize="large" />
-          <Typography variant="h6" sx={{ fontWeight: 550 }}>
-            Splitify
-          </Typography>
-        </IconButton>
-        <IconButton
-          size="large"
-          color="inherit"
-          edge="end"
-          onClick={handleLogout}
-        >
-          <LogoutRoundedIcon />
-        </IconButton>
-        <Avatar src={avatar} />
+        <Box display="flex" justifyContent="space-between" width="100%">
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="logo"
+            onClick={() => navigate("/")}
+          >
+            <AccountBalanceWalletRoundedIcon fontSize="large" />
+            <Typography variant="h6" sx={{ fontWeight: 550 }}>
+              Splitify
+            </Typography>
+          </IconButton>
+
+          <Box display="flex" flexDirection="row" alignItems="center" sx={{}}>
+            <Avatar src={avatar} sx={{marginRight: 1}}/>
+            <IconButton
+              size="large"
+              color="inherit"
+              edge="end"
+              onClick={handleLogout}
+            >
+              <LogoutRoundedIcon />
+            </IconButton>
+          </Box>
+        </Box>
       </Toolbar>
     </AppBar>
   );
