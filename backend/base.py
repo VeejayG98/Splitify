ebc18ae798fa2f1adee3b2c6c309959045974198
jayasuryaa-govindraj <jayasuryaa.govindraj1998@gmail.com>
2023-03-13 15:10:47 -0500

    Bug fixes.

--- backend/base.py
+++ backend/base.py
@@ -152,5 +152,6 @@ def addExpense():
 
     return "Failure", 400
 
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
