630d29021af1b041fd974be39185a2967c953608
jayasuryaa-govindraj <jayasuryaa.govindraj1998@gmail.com>
2023-03-15 00:23:45 -0500

    Modified the splitwise selection and added date selection.

--- frontend/src/components/SplitwiseForm.js
+++ frontend/src/components/SplitwiseForm.js
@@ -3,22 +3,77 @@ import {
   FormControl,
   FormControlLabel,
   FormLabel,
+  InputLabel,
+  MenuItem,
   Radio,
   RadioGroup,
+  Select,
 } from "@mui/material";
 import { Box } from "@mui/system";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import dayjs from "dayjs";
 
 export default function SplitwiseForm({
   commonGroups,
+  splitwiseGroup,
   setSplitwiseGroup,
   participants,
+  paidBy,
   setPaidBy,
-  addExpense
+  setExpenseDate,
 }) {
   return (
-    <Box display="flex" flexDirection="column" >
-      <Box display="flex" justifyContent="center">
-        <FormControl>
+    <Box display="flex" flexDirection="column">
+
+      <Box display="flex" justifyContent="space-between">
+
+
+        <Box display="flex" width="100%">
+          <FormControl sx={{minWidth: 200}}>
+            <InputLabel>Splitwise Group</InputLabel>
+            <Select
+              label="Splitwise Group"
+              onChange={(event) =>
+                setSplitwiseGroup(Number(event.target.value))
+              }
+              value={splitwiseGroup}
+            >
+              {commonGroups.map((group) => (
+                <MenuItem value={group.id} id={group.id}>
+                  {group.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Box>
+
+        <Box display="flex" width="100%">
+          <FormControl sx={{minWidth: 200}}>
+            <InputLabel>Paid by</InputLabel>
+            <Select
+              label="Paid by"
+              onChange={(event) => setPaidBy(Number(event.target.value))}
+              value={paidBy}
+              autoWidth
+            >
+              {[...participants].map((participant) => (
+                <MenuItem value={participant.id} id={participant.id}>
+                  {participant.first_name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Box>
+
+        <Box display="flex" minWidth={200}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker label="Date of expense" onChange={(date) => setExpenseDate(dayjs(date).format('YYYY-DD-MM'))}/>
+        </LocalizationProvider>
+      </Box>
+
+        {/* <FormControl>
           <FormLabel>Splitwise Group</FormLabel>
           <RadioGroup
             onChange={(event) => setSplitwiseGroup(Number(event.target.value))}
@@ -47,7 +102,7 @@ export default function SplitwiseForm({
               />
             ))}
           </RadioGroup>
-        </FormControl>
+        </FormControl> */}
       </Box>
       <Box display="flex" justifyContent="flex-end" marginBottom={10}>
         {/* <Button
